f35d7a5f356b218f68842d8c3913f82a7475d109
SsebasA <sebastian.antonio1707@gmail.com>
2026-05-26 21:08:21 -0600

    Step 10 Done

---
+++ ex_10_while.py
@@ -0,0 +1,17 @@
+from delta import Compiler, Phase
+
+source = '''
+    var n, r, i;
+    n = 5;
+    r = 1;
+    i = 0;
+    while n - i {
+        i = i + 1;
+        r = r * i;
+    }
+    r
+'''
+
+c = Compiler('program')
+c.realize(source)
+print(c.result)
